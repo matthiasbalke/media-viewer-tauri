8f4b47339a3111ff0bd030c47ecbad56c59e7e35
Matthias Balke <Matthias.Balke@googlemail.com>
2026-02-25 23:04:30 +0100

    add scripts to locally check coverage

---
+++ scripts/create-code-coverage-report.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+# Get the directory where the script is located
+SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+
+# Construct the path to the LCOV file
+LCOV_PATH="${SCRIPT_DIR}/../src-tauri/target/llvm-cov/lcov.info"
+
+# Create the code coverage report
+cd ${SCRIPT_DIR}/../src-tauri
+cargo llvm-cov --all-features --workspace --lcov --output-path ${LCOV_PATH}
